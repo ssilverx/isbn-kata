e172abd1653d472fa8840ace9f9051682b4138d9
Lars Eckart <lars.eckart@gmail.com>
2019-04-12 22:13:09 +0300

    extract versions of other dependencies too

--- src/test/java/bootstrap/AppTest.java
+++ src/test/java/bootstrap/AppTest.java
@@ -1,14 +1,15 @@
 package bootstrap;
 
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class AppTest {
 
     @Test
-    @DisplayName("My first test")
     void my_first_test() {
         assertThat("4" + "2").isEqualTo("42");
     }
