409a429da7f8b948f5aa6c83595ef82af37bfcb6
Lars Eckart <lars.eckart@gmail.com>
2020-03-11 11:35:24 +0200

    green

--- src/main/java/kata/ShoppingBasket.java
+++ src/main/java/kata/ShoppingBasket.java
@@ -1,18 +1,27 @@
 package kata;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class ShoppingBasket {
 
   private String string;
 
+  private List<Book> contents;
+
   public ShoppingBasket() {
     string = "0.00 EUR";
+    contents = new ArrayList<>();
   }
 
   public String total() {
-    return string;
+    int size = contents.size();
+    int total = size * 8;
+    return total + ".00 EUR";
   }
 
   public void put(Book book) {
+    contents.add(book);
     this.string = "8.00 EUR";
   }
 }
