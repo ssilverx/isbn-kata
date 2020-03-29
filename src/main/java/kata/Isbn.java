8c6514e0144330f3f886511c67e5b788c03150bb
Lars Eckart <lars.eckart@gmail.com>
2020-03-29 13:38:03 +0300

    refactoring

--- src/main/java/kata/Isbn.java
+++ src/main/java/kata/Isbn.java
@@ -1,12 +1,13 @@
 package kata;
 
 import java.util.ArrayList;
+import java.util.List;
 
 class Isbn {
 
-  static boolean isIsbn(String input) {
-    input = input.replace("-", "");
-    input = input.replace(" ", "");
+  static boolean isValid(String input) {
+    input = sanitize(input, "-");
+    input = sanitize(input, " ");
     if (input.length() != 13) {
       return false;
     }
@@ -16,19 +17,35 @@ class Isbn {
       return false;
     }
 
+    final List<Integer> integers = toIndividualIntegers(input);
+
+    int sum = sumOfProducts(integers);
+
+    int calculatedChecksum = (10 - (sum % 10)) % 10;
+
+    return calculatedChecksum == integers.get(12);
+  }
+
+  private static int sumOfProducts(List<Integer> integers) {
     int sum = 0;
-    final ArrayList<Integer> integers = new ArrayList<>();
-    for (char inputChar : input.toCharArray()) {
-      integers.add(Character.getNumericValue(inputChar));
-    }
     for (int i = 0; i < integers.size() - 1; i = i + 2) {
       sum += integers.get(i);
     }
     for (int i = 1; i < integers.size() - 1; i = i + 2) {
       sum += integers.get(i) * 3;
     }
-    int result = (10 - (sum % 10)) % 10;
+    return sum;
+  }
 
-    return result == integers.get(12);
+  private static List<Integer> toIndividualIntegers(String input) {
+    final List<Integer> integers = new ArrayList<>();
+    for (char inputChar : input.toCharArray()) {
+      integers.add(Character.getNumericValue(inputChar));
+    }
+    return integers;
+  }
+
+  private static String sanitize(String input, String s) {
+    return input.replace(s, "");
   }
 }
--- src/test/java/kata/AppTest.java
+++ src/test/java/kata/AppTest.java
@@ -11,31 +11,31 @@ class AppTest {
 
   @Test
   void invalid_isbn() {
-    assertThat(Isbn.isIsbn("978047005902")).isFalse();
+    assertThat(Isbn.isValid("978047005902")).isFalse();
   }
 
   @Test
   void invalid_isbn_more_than_13_digits() {
-    assertThat(Isbn.isIsbn("97804700590291")).isFalse();
+    assertThat(Isbn.isValid("97804700590291")).isFalse();
   }
 
   @Test
   void invalid_isbn_when_input_contains_non_digits() {
-    assertThat(Isbn.isIsbn("978047005902a")).isFalse();
+    assertThat(Isbn.isValid("978047005902a")).isFalse();
   }
 
   @Test
   void valid_isbn_can_contain_dashes() {
-    assertThat(Isbn.isIsbn("978-0596809485")).isTrue();
+    assertThat(Isbn.isValid("978-0596809485")).isTrue();
   }
 
   @Test
   void valid_isbn_can_contain_spaces() {
-    assertThat(Isbn.isIsbn("978 0 471 48648 0")).isTrue();
+    assertThat(Isbn.isValid("978 0 471 48648 0")).isTrue();
   }
 
   @Test
   void valid_isbn_needs_correct_checksum() {
-    assertThat(Isbn.isIsbn("9780470059020")).isFalse();
+    assertThat(Isbn.isValid("9780470059020")).isFalse();
   }
 }
